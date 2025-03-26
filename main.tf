b113a7cff191ac3b68b987d01965e338327f635a
joshua.beckstrand <joshua.beckstrand@utahtech.edu>
2025-03-26 16:23:50 -0600

    cloud fix again

--- main.tf
+++ main.tf
@@ -91,4 +91,12 @@ resource "aws_eip" "example" {
   instance = aws_instance.example.id
 }
 
+output "instance_public_ip" {
+  value = aws_eip.example.public_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.example_sg.id
+}
+
 
